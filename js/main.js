6caf1f2df90e32e574613c4bcf8332707ebd9d43
Chinmay <chinmayrane17@gmail.com>
2015-02-05 14:10:27 +0530

    Version 0.0.1

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+var grabstr = grabstr || {};
+
+grabstr.homePage = (function($, doc, win, undefined){
+
+	var init = function(){
+
+	}();
+
+
+		var solution = {
+			init : init
+		};
+
+		return solution;
+
+})(jQuery,document,window);	
